cd564394303fa99103f869f03cc7b1088a52f1c8
navvatart <146699264+Navvatart@users.noreply.github.com>
2024-05-01 13:20:34 +0700

    Update install-genpro.sh

--- install-genpro.sh
+++ install-genpro.sh
@@ -1,1 +1,28 @@
+#!/bin/ash
+# Installation script by @Navvatart.
 
+DIR=/usr/bin
+
+finish(){
+    clear
+    echo ""
+    echo "DOWNLOAD GENPRO PACKAGE MANAGER DONE"
+    echo ""
+    echo "this tools for Openclash only. generate many proxy and SNI with one click"
+    echo ""
+    echo ""
+}
+
+download_files()
+{
+    clear
+    echo "Please Wait"
+    sleep 5
+    echo "Internet connection must ready"
+    sleep 5
+    echo "Downloading In Progress..... Please Wait"
+    cd www/ && wget https://github.com/Navvatart/genpro/archive/refs/tags/Beta-v01.05.01.2024.zip && unzip Beta-v01.05.01.2024.zip && rm Beta-v01.05.01.2024.zip && mv genpro-Beta-v01.05.01.2024 genpro && cd -
+    finish
+}
+
+download_files
