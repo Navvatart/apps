57b2822dc0e7b077308a4b9f39e5bb05620dde5c
Navvatart <146699264+Navvatart@users.noreply.github.com>
2024-05-14 21:23:04 +0700

    Update install-genpro.sh

--- install-genpro.sh
+++ install-genpro.sh
@@ -51,14 +51,17 @@ download_files()
     echo "Internet connection must ready"
     sleep 5
     echo "Downloading In Progress..... Please Wait"
-    [ ! -d "/www/genpro" ] &&
-     	echo "kosong";
-    	sleep 5
-	#cd www/ && wget https://github.com/Navvatart/genpro/archive/refs/tags/Beta-v01.05.01.2024.zip && unzip Beta-v01.05.01.2024.zip && rm Beta-v01.05.01.2024.zip && mv genpro-Beta-v01.05.01.2024 genpro && cd -
-    ||
-    	echo "ada";
-    	sleep 5
+    if [ -d "/www/genpro" ]
+    then
+        echo "a is less than b"
+	sleep 5
+    else
+        echo "a is greater than b"
+	sleep 5
     fi
+   
+	#cd www/ && wget https://github.com/Navvatart/genpro/archive/refs/tags/Beta-v01.05.01.2024.zip && unzip Beta-v01.05.01.2024.zip && rm Beta-v01.05.01.2024.zip && mv genpro-Beta-v01.05.01.2024 genpro && cd -
+   
     finish
 }
 
